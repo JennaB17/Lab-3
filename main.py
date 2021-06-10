4836bdebe5e0b42d715e650f98f6c007d779b4cf
JennaB17 <83236217+JennaB17@users.noreply.github.com>
2021-06-10 16:32:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+#Part A Function 1
+
+#a = input()
+#list1 = []
+
+#for numbers in a.split():
+  #if numbers.isdigit() == True:
+ #   list1.append(int(numbers))
+#print(list1)
+
+#list2 = []
+#for words in a.split():
+ # if words.isdigit() == False:
+  #  list2.append(str(words))
+#print(list2)
+
+
+# Function 2
+#a = input()
+#reverse = range[0::-2]
+#print(reverse(a))
+
+
+
+
+def reverse(list1):
+  for i in range(list1, 0, -2):
+    print(i)
+
+a = input()
